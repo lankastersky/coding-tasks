dbeca06b8820f979844eda660aa2ed42b3ab7a90
Anton Popov <lankastersky@gmail.com>
2019-08-07 20:46:16 -0700

    Update knight-probability-in-chessboard.java

--- dynamic-programming/knight-probability-in-chessboard.java
+++ dynamic-programming/knight-probability-in-chessboard.java
@@ -1,4 +1,6 @@
 /*
+Knight Probability in Chessboard
+
 On an NxN chessboard, a knight starts at the r-th row and c-th column and attempts to make exactly K moves. 
 The rows and columns are 0 indexed, so the top-left square is (0, 0), and the bottom-right square is (N-1, N-1).
 
@@ -26,6 +28,8 @@ Note:
 N will be between 1 and 25.
 K will be between 0 and 100.
 The knight always initially starts on the board.
+
+https://leetcode.com/problems/knight-probability-in-chessboard/
 */
 
 class Solution {
