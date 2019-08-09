ea1c013363ff256416e0914281096bbb7b25d289
Anton Popov <lankastersky@gmail.com>
2019-08-08 17:58:22 -0700

    Update flower-planting-with-no-adjacent.java

--- graphs/flower-planting-with-no-adjacent.java
+++ graphs/flower-planting-with-no-adjacent.java
@@ -36,3 +36,72 @@ It is guaranteed an answer exists.
 leetcode - Google online assessment
 */
 
+class Solution {
+    
+    static final int COL = 4;
+    Map<Integer, ArrayList<Integer>> adj;
+    int[] flowers;
+    
+    public int[] gardenNoAdj(int N, int[][] paths) {
+        adj = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            adj.put(i, new ArrayList<>());
+        }
+        
+        fillAdj(N, paths);
+        
+        flowers = new int[N];
+        for (int i = 0; i < N; i++) {
+            rec(i);
+        }
+        return flowers;
+    }
+    
+    boolean rec(int cur) {
+        if (flowers[cur] != 0) {
+            // visited
+            return true;
+        }
+        boolean[] fl = new boolean[COL + 1];
+        ArrayList<Integer> neib = adj.get(cur);
+        if (neib == null) {
+            return false;
+        }
+        if (neib.isEmpty()) {
+            flowers[cur] = 1;
+            return true;
+        }
+        
+        // find flowers of neighbours
+        for (int n : neib) {
+            int c = flowers[n];
+            if (c != 0) {
+                fl[c] = true;
+            }
+        }
+        for (int c = 1; c <= COL; c++) {
+            if (!fl[c]) {
+                flowers[cur] = c;
+                boolean res = true;
+                for (int n : neib) {
+                    if (!rec(n)) {
+                        res = false;
+                        break;
+                    }
+                }
+                if (res) {
+                    return true;
+                }
+                flowers[cur] = 0;
+            }
+        }
+        return false;
+    }
+    
+    void fillAdj(int N, int[][] paths) {
+        for (int[] p : paths) {
+            adj.get(p[0] - 1).add(p[1] - 1);
+            adj.get(p[1] - 1).add(p[0] - 1);
+        }
+    }
+}
