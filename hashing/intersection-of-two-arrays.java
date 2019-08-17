927c729761ef5d7d81c560dc67648c7ab1a6de25
Anton Popov <lankastersky@gmail.com>
2019-08-16 20:15:35 -0700

    Create intersection-of-two-arrays.java

---
+++ hashing/intersection-of-two-arrays.java
@@ -0,0 +1,33 @@
+/*
+Intersection of Two Arrays
+
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Note:
+
+Each element in the result must be unique.
+The result can be in any order.
+
+https://leetcode.com/problems/intersection-of-two-arrays/
+*/
+
+import java.util.*;
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> first = new HashSet<>(
+            Arrays.stream(nums1).boxed().collect(Collectors.toList()));
+        Set<Integer> second = new HashSet<>(
+            Arrays.stream(nums2).boxed().collect(Collectors.toList()));
+        first.retainAll(second);
+        return first.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
