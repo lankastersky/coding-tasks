dfe1b7576d192a718436b711e832e668656de4fb
Anton Popov <lankastersky@gmail.com>
2019-10-13 20:01:51 -0700

    Create accounts-merge.java

---
+++ graphs/accounts-merge.java
@@ -0,0 +1,92 @@
+/*
+Accounts Merge
+
+Given a list accounts, each element accounts[i] is a list of strings, where the first element 
+accounts[i][0] is a name, and the rest of the elements are emails representing emails of the account.
+
+Now, we would like to merge these accounts. Two accounts definitely belong to the same person if 
+there is some email that is common to both accounts. Note that even if two accounts have the same
+name, they may belong to different people as people could have the same name. A person can have 
+any number of accounts initially, but all of their accounts definitely have the same name.
+
+After merging the accounts, return the accounts in the following format: the first element of each 
+account is the name, and the rest of the elements are emails in sorted order. The accounts themselves
+can be returned in any order.
+
+Example 1:
+Input: 
+accounts = [["John", "johnsmith@mail.com", "john00@mail.com"], ["John", "johnnybravo@mail.com"], ["John", "johnsmith@mail.com", "john_newyork@mail.com"], ["Mary", "mary@mail.com"]]
+Output: [["John", 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com'],  ["John", "johnnybravo@mail.com"], ["Mary", "mary@mail.com"]]
+Explanation: 
+The first and third John's are the same person as they have the common email "johnsmith@mail.com".
+The second John and Mary are different people as none of their email addresses are used by other accounts.
+We could return these lists in any order, for example the answer [['Mary', 'mary@mail.com'], ['John', 'johnnybravo@mail.com'], 
+['John', 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com']] would still be accepted.
+
+Note:
+
+The length of accounts will be in the range [1, 1000].
+The length of accounts[i] will be in the range [1, 10].
+The length of accounts[i][j] will be in the range [1, 30].
+
+https://leetcode.com/problems/accounts-merge/
+*/
+
+class Solution {
+  public List<List<String>> accountsMerge(List<List<String>> accounts) {
+    // Stores email to account numbers. It could be much simpler to just 
+    // store email to list of emails but I realized it too late.
+    Map<String, ArrayList<Integer>> map = new HashMap<>();
+    for (int i = 0; i < accounts.size(); i++) {
+      List<String> emails = accounts.get(i);
+      for (int j = 1; j < emails.size(); j++) {
+        String email = emails.get(j);
+        ArrayList<Integer> acs = map.getOrDefault(email, new ArrayList<>());
+        acs.add(i);
+        map.put(email, acs);
+      }
+    }
+    List<List<String>> res = new ArrayList<>();
+    bfs(accounts, map, res);
+    return res;
+  }
+  
+  void bfs(List<List<String>> accounts, 
+           Map<String, ArrayList<Integer>> graph, 
+           List<List<String>> res) {
+    
+    Queue<String> queue = new LinkedList<>();
+    Set<String> visited = new HashSet<>();
+    for (String email : graph.keySet()) {
+      if (visited.contains(email)) {
+        continue;
+      }
+      ArrayList<String> merged = new ArrayList<>();
+
+      queue.add(email);
+      visited.add(email);
+      
+      while (!queue.isEmpty()) {
+        email = queue.remove();
+        merged.add(email);
+        ArrayList<Integer> acNumbers = graph.get(email);
+        for (int i = 0; i < acNumbers.size(); i++) {
+          List<String> emails = accounts.get(acNumbers.get(i));
+          for (int j = 1; j < emails.size(); j++) {
+            String em = emails.get(j);
+            if (visited.contains(em)) {
+              continue;
+            }
+            visited.add(em);
+            queue.add(em);
+          }
+        }
+      }
+      Collections.sort(merged);
+      ArrayList<Integer> acNumbers = graph.get(email);
+      String acName = accounts.get(acNumbers.get(0)).get(0);
+      merged.add(0, acName);
+      res.add(merged);
+    }
+  }
+}
